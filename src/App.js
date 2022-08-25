5f16e404ddee57caa11309714e33d566215e605e
Damien <80352555+Twismdx@users.noreply.github.com>
2022-08-25 22:55:51 +0930

    main

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAZod7qTfZu4h2M644X5YZ8tRYl6_MHEJ4",
+  authDomain: "t140scoreboard.firebaseapp.com",
+  projectId: "t140scoreboard",
+  storageBucket: "t140scoreboard.appspot.com",
+  messagingSenderId: "4014256889",
+  appId: "1:4014256889:web:d02548847cc593551ea828"
+};
 
-export default App;
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
